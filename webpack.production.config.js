9b28d2116d4371641cc381160f0159623ea09b24
Jimmy Scray <jimmy.scray@uber.com>
2017-10-06 16:23:52 -0700

    removing react addons

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -15,7 +15,6 @@ module.exports = {
     //but get it from a global 'React' variable
     
     'react': 'react',
-    'react/addons': true,
     'react-dom': 'react-dom',
     'react-datepicker': 'react-datepicker',
     'classnames': 'classnames',
